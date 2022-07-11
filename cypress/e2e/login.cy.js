c98c5a3a5339944e3d6bd71cd624e5f363d2793d
Bartosz <82472210+BaKozak@users.noreply.github.com>
2022-07-11 22:16:14 +0200

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,8 @@
-const { italic } = require("colorette")
 
 describe('Login and tests on My Profile Developer', () => {
 
   beforeEach('logging in', () => {
     cy.loginToApp()
   })
-
-  italic('')
+  
 })
