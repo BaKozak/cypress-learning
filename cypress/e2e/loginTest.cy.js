27c2e6d24207b9d9a3aab256d6b0836225fc074e
Bartosz <82472210+BaKozak@users.noreply.github.com>
2022-07-12 09:30:33 +0200

    Comment
    adding comments

--- cypress/e2e/loginTest.cy.js
+++ cypress/e2e/loginTest.cy.js
@@ -16,9 +16,9 @@ describe('Login and positive tests on My Profile Developer', () => {
         cy.wrap(acDet).find('#JavaScript').click()
         cy.wrap(acDet).find('#Management').click()
         // cy.wrap(acDet).find(`#C++`).click() //cos nie znajduje id
-        // cy.wrap(acDet).find('#Research skills').click()
-        // cy.wrap(acDet).find('#Strategic thinking').click()
-        cy.wrap(acDet).find('.lc-multiselect-head').type('{selectall}{backspace}') //tylko usuwa jedno
+        // cy.wrap(acDet).find('#Research skills').click() // ta sam sytuacja co pozywej tylko podejrzewam ze to wina spacji?
+        // cy.wrap(acDet).find('#Strategic thinking').click() // ta sama sytuacja ze spacja jest
+        cy.wrap(acDet).find('.lc-multiselect-head').type('{selectall}{backspace}')  //tylko usuwa jedno
       })
     })
   })
