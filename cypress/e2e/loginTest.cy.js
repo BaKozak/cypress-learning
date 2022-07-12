b7076da13f8beaf500aa196918ec477fffdb67d0
Bartosz <82472210+BaKozak@users.noreply.github.com>
2022-07-12 02:46:47 +0200

    Update
    Historia

--- cypress/e2e/loginTest.cy.js
+++ cypress/e2e/loginTest.cy.js
@@ -1,8 +1,25 @@
 
-describe('Login and tests on My Profile Developer', () => {
+describe('Login and positive tests on My Profile Developer', () => {
 
     beforeEach('logging in', () => {
       cy.loginToApp()
+    })
+
+    it('accessing My Developer Profile', () => {
+      cy.get('.css-1v1v4wl').find('.avatar').click()
+      cy.get('.css-18i1gyy').first().click()
+      cy.get('.css-96qgqv').eq(1).then( acDet  => {
+        cy.wrap(acDet).find('#name').clear().type('Creed Bratton')
+        cy.wrap(acDet).find('#jobTitle').clear().type('Quabity Ashuance')
+        cy.wrap(acDet).find('#company').clear().type('Paper Company')
+        cy.wrap(acDet).find('#skills').click()
+        cy.wrap(acDet).find('#JavaScript').click()
+        cy.wrap(acDet).find('#Management').click()
+        // cy.wrap(acDet).find(`#C++`).click() //cos nie znajduje id
+        // cy.wrap(acDet).find('#Research skills').click()
+        // cy.wrap(acDet).find('#Strategic thinking').click()
+        cy.wrap(acDet).find('.lc-multiselect-head').type('{selectall}{backspace}') //tylko usuwa jedno
+      })
     })
   })
   
